808573b93b35ed0cc60e9d6a218d30a655ac3e55
robles153 <marcoskim153@gmail.com>
2023-01-27 16:12:02 -0300

    Criando Models

---
+++ Order.Domainn/Models/OrderModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Order.Domain.Models
+{
+    public class OrderModel : EntityBase
+    {
+        public ClientModel Client { get; set; }
+        public UserModel User { get; set; }
+
+    }
+}
