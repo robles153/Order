517e8bee14a72be9ca58c4a48a29889bd7eeb659
robles153 <marcoskim153@gmail.com>
2023-01-28 11:29:28 -0300

    Criando Interfaces dos Repositorios

--- Order.Domainn/Models/OrderModel.cs
+++ Order.Domainn/Models/OrderModel.cs
@@ -1,9 +1,12 @@
-﻿namespace Order.Domain.Models
+﻿using System.Collections.Generic;
+
+namespace Order.Domain.Models
 {
     public class OrderModel : EntityBase
     {
         public ClientModel Client { get; set; }
         public UserModel User { get; set; }
+        public List<OrderItemModel> Items { get; set; }
 
     }
 }
