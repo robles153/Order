900210fec38714180b1c2508710fc841c281c932
robles153 <marcoskim153@gmail.com>
2023-02-01 17:25:33 -0300

    Criando response

---
+++ Order.Domainn/Validations/Base/Response.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Order.Domain.Validations.Base
+{
+    public class Response
+    {
+        public Response()
+        {
+            Reports = new List<Report>();
+        }
+
+        public Response(List<Report> reports)
+        {
+            Reports = reports;
+        }
+
+        public Response(Report report) : this(new List<Report>() { report})
+        {
+
+        }
+
+        public List<Report> Reports { get; }
+    }
+
+    public class Response<T> : Response
+    {
+        public Response()
+        {
+
+        }
+
+        public Response(T data, List<Report> reports = null) : base(reports)
+        {
+            Data = data;
+        }
+
+        public T Data { get; set; }
+    }
+}
