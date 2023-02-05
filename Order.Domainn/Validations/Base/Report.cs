2fda85989c22ae64aa3c4d0201b4b67b7a3dcdd9
robles153 <marcoskim153@gmail.com>
2023-02-05 13:12:18 -0300

    Implementando clientservice

--- Order.Domainn/Validations/Base/Report.cs
+++ Order.Domainn/Validations/Base/Report.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Order.Domain.Validations.Base
 {
     public class Report
     {
+        public Report()
+        {
+
+        }
+
+        public Report(string message)
+        {
+            Message = message;
+        }
+
         public string Code { get; set; }
         public string Message { get; set; }
+
+        public static Report Create(string message) => new Report(message);
     }
 }
